b21a1ab3db3097c516622ceb4540adf6bf8d5e1b
Harmedino <adamsahmed191@gmail.com>
2024-02-18 20:57:03 +0100

    initial commit

---
+++ backend/config/firebase.config.js
@@ -0,0 +1,19 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// const firebase = require('firebase');
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+        apiKey: "AIzaSyDadLjKCXfzDYkZHPMNn1s0FGNup6I9xFE",
+        authDomain: "expense-app-4978b.firebaseapp.com",
+        projectId: "expense-app-4978b",
+        storageBucket: "expense-app-4978b.appspot.com",
+        messagingSenderId: "316441528452",
+        appId: "1:316441528452:web:c6a68c4f2375effe85dad1",
+        measurementId: "G-PM4Y362CVJ"
+}
+    const app = initializeApp(firebaseConfig);
+
+      
+export default app 
